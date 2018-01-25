91b61bfaca562d9b3d630ea7382c5af296b4ca6f
njoel24 <cristiano.anselmi@gmail.com>
2018-01-26 00:28:56 +0100

    epic: it works!

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
 import * as redux from 'redux';
-
 import { compose } from '../utils'
 import * as state from '../reducers/reducer';
 
@@ -35,34 +34,65 @@ interface ConnectedState {
 
 
 export class MyAppClass extends React.Component<any> {
-    state: any = {value1: "", value2: ""};
+    state: any = {value1: [], value2: []};
     allCountries: any;
+
     change(event: any) {
-      if (event.target.name === "nation1") {
-        this.setState({value1: event.target.value});
+      const value = event.target.value;
+      const name = event.target.name
+      if (name === "nation1") {
+        this.setState({value1: this.getCountryByCountryCode(value)});
       } else {
-        this.setState({value2: event.target.value});
+        this.setState({value2: this.getCountryByCountryCode(value)});
       }
     }
+
+    getCountryByCountryCode(countryCode: string): any {
+      return this.allCountries.filter((country: any) => {
+        return country.countryCode === countryCode ? country: null
+      });
+    }
+
+    getCountryData(country: any) {
+      if (country) {
+        return <div className="details">
+           <span>Country Code:</span><span>{country["countryCode"]}</span>
+           <span>Currency Code:</span><span>{country["currencyCode"]}</span>
+           <span>Population:</span><span>{country["population"]}</span>
+           <span>Capital:</span><span>{country["capital"]}</span>
+           <span>Continent Name:</span><span>{country["continentName"]}</span>
+        </div>
+      }
+    }
+
+    componentDidUpdate(){
+      this.getCountryData(this.state.value1[0]);
+      this.getCountryData(this.state.value2[0]);
+    }
+
+    getSelect(nation: string) {
+      return <select name={nation} onChange={this.change.bind(this)}> 
+      <option value="default">Choose</option>
+      {this.allCountries.map( (country: any) =>
+        <option value={country.countryCode} key={country.countryCode}>{country.name}</option>
+      )}
+      </select>
+    }
+    
     render() {
-       this.allCountries = this.props
+       this.allCountries = this.props.allCountries;
+       if (!this.allCountries) {
+         return null;
+       }
         return <div className="container">
-        <div> 
-        <select name="nation1" onChange={this.change.bind(this)}> 
-            {this.allCountries.map( (country: any) =>
-              <option value={country.countryCode} key={country.countryCode}>{country.name}</option>
-            )}
-        </select>
-        <span>{this.state.value1}</span>
-        </div>
-        <div>
-        <select name="nation2" onChange={this.change.bind(this)}> 
-          {this.allCountries.map( (country: any) =>
-            <option value={country.countryCode} key={country.countryCode}>{country.name}</option>
-          )}
-        </select>
-        <span>{this.state.value2}</span>
-        </div>
+          <div className="firstSelect"> 
+            {this.getSelect("nation1")}
+            {this.getCountryData(this.state.value1[0])}
+          </div>
+          <div className="secondSelect">
+            {this.getSelect("nation2")}
+            {this.getCountryData(this.state.value2[0])}
+          </div>
         </div>;
     }
 }
